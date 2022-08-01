e64062bd7abea94592a18cf3bda70b53642f7815
Marcos <marcoszinga_1994@hotmail.com>
2022-08-01 08:40:10 -0300

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { CapacidadesComponent } from './components/capacidades/capacidades.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    CapacidadesComponent,
+    FooterComponent,
+    PortafolioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
