bef020af659968de9edf7991d8a63417d6294517
Zinike <marcoszinga_1994@hotmail.com>
2022-08-31 21:47:58 -0300

    asd

--- src/app/servicios/portafolio.service.ts
+++ src/app/servicios/portafolio.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 import { PortafolioModel } from '../models/portafolio';
 
 @Injectable({
@@ -9,7 +8,7 @@ import { PortafolioModel } from '../models/portafolio';
 })
 export class PortafolioService {
   
-  private apiServerUrl=environment.apiBaseUrl;
+  private apiServerUrl='http://localhost:8080';
   
   constructor(private http:HttpClient) { }
 
